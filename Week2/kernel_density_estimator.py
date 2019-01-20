0c2f8bbba381032ff846b9354a507569c6518f57
Eric Valentino <evalentino@mail.usf.edu>
2019-01-19 20:28:01 -0500

    implementing kernel density estimator maybe

--- Week2/kernel_density_estimator.py
+++ Week2/kernel_density_estimator.py
@@ -1,6 +1,19 @@
 #%%
+import numpy as np
+import matplotlib.pyplot as plt
 pi = 3.14156
 e = 2.71828
+## getting data
+def get_data():
+    data = []
+    for fname in ["data{}.txt".format(i) for i in range(1,7)]:
+        with open(fname,'r') as f:
+            this_data = []
+            for line in f.readlines():
+                this_data.append(float(line))
+                assert not this_data[-1] == np.nan
+            data.append(this_data)
+
 ##Windowing Functions 
 def gaussian(t,h):
     return 1/(h*(2*pi)**0.5)*e**(-t**2/(2*h**2))
@@ -17,6 +30,37 @@ def rectangle(t,h):
     else:
         return 1/(2*h)
 
-def kernel_density_estimator(data,h,num_pts = 200,return_function=False):
+def kernel_density_estimator(data,h,f=None,num_pts = 200,return_f=False):
+    '''
+    `data`      -- 1-D vector of numpy data
+    `h`         -- smoothing bandwidth
+    `f`         -- windowing function
+    `num_pts`   -- number of points to use to show trace
+    `return_f`  -- whether or not to return a function rather than trace data
+    '''
     assert h>0 , "Bandwidth must be a positive number"
+    if f is None: # default function is epanechnikov
+        f = epanechnikov
+    N = len(data)
+    x_pts = np.linspace(np.amin(data),np.amax(data),num_pts)
+    return np.array([(1/N)*np.sum(f(data-x,h)) for x in x_pts]),x_pts # I could have made this a one liner... Forgive me Guido for I have sinned
 
+#%%
+data = get_data
+#%%
+# epannechnikov? I hardly know her
+x,y = kernel_density_estimator(data[0],.01)
+plt.hist(data[0],'o')
+plt.plot(x,y)
+        
+#%%
+# Gaussian
+x,y = kernel_density_estimator(data[0],.01,f=gaussian)
+plt.hist(data[0],'o')
+plt.plot(x,y)
+
+#%%
+# 
+x,y = kernel_density_estimator(data[0],.01,f=rectangle)
+plt.hist(data[0],'o')
+plt.plot(x,y)
