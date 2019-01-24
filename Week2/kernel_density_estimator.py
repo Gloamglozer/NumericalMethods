eddf4ac0a3504ec51ec7fa1d09cd4175fbd92738
Eric Valentino <evalentino@mail.usf.edu>
2019-01-24 00:00:37 -0500

    Fixing kernel density estimator

--- Week2/kernel_density_estimator.py
+++ Week2/kernel_density_estimator.py
@@ -1,6 +1,7 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
+
 pi = 3.14156
 e = 2.71828
 ## getting data
@@ -13,22 +14,23 @@ def get_data():
                 this_data.append(float(line))
                 assert not this_data[-1] == np.nan
             data.append(this_data)
+    return np.array(data)
 
 ##Windowing Functions 
 def gaussian(t,h):
     return 1/(h*(2*pi)**0.5)*e**(-t**2/(2*h**2))
 
 def epanechnikov(t,h):
-    if abs(t/h) > 5**0.5:
-        return 0 
-    else:
-        return (3/4)*(1-(1/5)*(t/h)**2)/5**0.5
+    # note numpy.where serves to vectorize this function scalars must be converted to numpy arrays or else the function will fail
+    t = np.asarray(t)
+    h = np.asarray(h)
+    return np.where(abs(t/h) > 5**0.5,0,(3/4)*(1-(1/5)*(t/h)**2)/5**0.5)
 
 def rectangle(t,h):
-    if abs(t) > h:
-        return 0 
-    else:
-        return 1/(2*h)
+    # note numpy.where serves to vectorize this function. scalars must be converted to numpy arrays or else the function will fail
+    t = np.asarray(t)
+    h = np.asarray(h)
+    return np.where( abs(t) > h, 0 ,1/(2*h))
 
 def kernel_density_estimator(data,h,f=None,num_pts = 200,return_f=False):
     '''
@@ -43,24 +45,34 @@ def kernel_density_estimator(data,h,f=None,num_pts = 200,return_f=False):
         f = epanechnikov
     N = len(data)
     x_pts = np.linspace(np.amin(data),np.amax(data),num_pts)
-    return np.array([(1/N)*np.sum(f(data-x,h)) for x in x_pts]),x_pts # I could have made this a one liner... Forgive me Guido for I have sinned
+    return x_pts,np.array([(1/N)*np.sum(f(data-x,h)) for x in x_pts]) # I could have made this a one liner... Forgive me Guido for I have sinned
 
 #%%
-data = get_data
+data = get_data()
+which_data = 2
+num_pts = 1000
+
 #%%
 # epannechnikov? I hardly know her
-x,y = kernel_density_estimator(data[0],.01)
-plt.hist(data[0],'o')
-plt.plot(x,y)
+fig,ax1 =plt.subplots()
+ax2 = ax1.twinx()
+x,y = kernel_density_estimator(data[which_data],.1,num_pts=num_pts)
+ax1.hist(data[which_data])
+ax2.plot(x,y,color='k')
+plt.show()
         
-#%%
 # Gaussian
-x,y = kernel_density_estimator(data[0],.01,f=gaussian)
-plt.hist(data[0],'o')
-plt.plot(x,y)
-
+fig,ax1 =plt.subplots()
+ax2 = ax1.twinx()
+x,y = kernel_density_estimator(data[which_data],.1,f=gaussian,num_pts=num_pts)
+ax1.hist(data[which_data])
+ax2.plot(x,y,color='k')
+plt.show()
 #%%
 # 
-x,y = kernel_density_estimator(data[0],.01,f=rectangle)
-plt.hist(data[0],'o')
-plt.plot(x,y)
+fig,ax1 =plt.subplots()
+ax2 = ax1.twinx()
+x,y = kernel_density_estimator(data[which_data],.1,f=rectangle,num_pts=num_pts)
+ax1.hist(data[which_data])
+ax2.plot(x,y,color='k')
+plt.show()
