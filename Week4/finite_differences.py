12ae3107b3b3fe10c4cc1746b221204c420dc9a6
Eric Valentino <evalentino@mail.usf.edu>
2019-02-08 13:18:41 -0500

    Small changes made in class

--- Week4/finite_differences.py
+++ Week4/finite_differences.py
@@ -2,33 +2,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%%
-cond_S_per_cm = 1000 #
-size = 100 # number of divisions used for finite difference
-boundary_right = 0
-boundary_left = 1
-
-
-A = np.empty((size,size))
-
-#toeplitz-izing empty matrix
-for i in range(size):
-    A[i,i] = 2
-    if i ==size-1:
-        break
-    A[i,i+1] = -1
-    A[i+1,i] = -1
-
-
-b = np.empty((size,1))
-
-b[0,0] = boundary_left
-b[size-1,0] = boundary_right
-
-x = np.linalg.lstsq(A,b)
-plt.plot(x)
-
-
 #%% [markdown]
-## Blah
-# Problem 1: Plot the voltage as a function of position
+## Problem 1: Plot the voltage as a function of position
+# This was solved using the normal (non-sparse) NumPy array
+
+#%%
+def solve_for_voltage(size,boundaries,cond_S_per_cm= 1000):
+    ''' 
+    `size` - number of divisions used to discretize line
+    `boundaries` - tuple containing voltage on bounds like `(V_left,V_right)`
+    `cond_S_per_cm` - conductivity
+    '''
+
+    boundary_left ,boundary_right = boundaries
+
+    A = np.zeros((size,size))
+
+    #toeplitz-izing empty matrix
+    for i in range(size):
+        A[i,i] = 2
+        if i ==size-1:
+            break
+        A[i,i+1] = -1
+        A[i+1,i] = -1
+
+    b = np.zeros((size,1))
+
+    b[0,0] = boundary_left
+    b[size-1,0] = boundary_right
+
+    return np.linalg.solve(A,b) # equivalent of MATLAB backslash
+
+x = solve_for_voltage(100,(1,0))
+plt.plot(x)
+#%%
+import memory_profiler
+
+N_list = np.floor(np.logspace(1,3,10))
+for N in N_list:
+    #%memit solve_for_voltage(100,(1,0))
+    pass
